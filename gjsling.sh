772c6ea0a1ada840f4bb7aaa9d74082fa3fd5b37
pc <pc@pc.(none)>
2011-06-12 21:42:35 +0900

    add gjslint check script

---
+++ gjsling.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+js="js/jquery.belatedPNG.js"
+
+
+gjslint --strict ${js} | sed -n '/^Line/p'  | while read line
+do
+	if ! expr "${line}" : "^Line.*\?E:0110" >/dev/null ; then
+		echo ${line}
+	fi
+done
