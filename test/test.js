1df9956ec048a82e8e4774dbfd01c91ddf247582
pc <pc@pc.(none)>
2011-06-19 21:06:53 +0900

    add test createVmlNameSpace

--- test/test.js
+++ test/test.js
@@ -1,3 +1,20 @@
 (function($) {
 
+module("setup test");
+
+test("createVmlNameSpace", function() {
+  var ns = 'DD_belatedPNG',
+    nsDD_belatedPNG;
+
+  if (document.namespaces) {
+    expect(3);
+    nsDD_belatedPNG = document.namespaces[ns];
+    ok(nsDD_belatedPNG);
+    equals(nsDD_belatedPNG.name, ns);
+    equals(nsDD_belatedPNG.urn, 'urn:schemas-microsoft-com:vml');
+  } else {
+      expect(0);
+  }
+});
+
 })(jQuery);
